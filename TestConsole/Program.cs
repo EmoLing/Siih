dbab40753db1ac6e71ce3a7a731a91beb0573030
Артем Селиванов <temchik1997@yandex.ru>
2025-02-10 18:34:53 +0300

    TestConsole: Косольное приложение для генерации данных DB: переделал генерацию БД. Указал абстрактность объекта. Добавил для пользователя список оборудования

---
+++ TestConsole/Program.cs
@@ -0,0 +1,45 @@
+﻿using Bogus;
+using DB;
+using DB.Models.Departments;
+using DB.Models.Equipment;
+using DB.Models.Users;
+
+using var db = new ApplicationDBContext();
+
+var fakerUser = new Faker<User>("ru")
+    .RuleFor(u => u.FirstName, f => f.Name.FirstName())
+    .RuleFor(u => u.LastName, f => f.Name.LastName())
+    .RuleFor(u => u.Surname, f => f.Name.Prefix())
+    .RuleFor(u => u.Name, f => f.Name.FullName());
+
+var fakerDepartment = new Faker<Department>("ru")
+    .RuleFor(d => d.Name, f => f.Commerce.Department());
+
+var fakerJobTitle = new Faker<JobTitle>("ru")
+    .RuleFor(jb => jb.Name, f => f.Name.JobTitle());
+
+var fakerSoftware = new Faker<Software>()
+    .RuleFor(s => s.Version, f => f.System.Version().ToString())
+    .RuleFor(s => s.Name, f => f.Commerce.ProductName());
+
+var fakerHardware = new Faker<Hardware>()
+    .RuleFor(h => h.Name, h => h.Commerce.ProductName())
+    .RuleFor(h => h.SerialNumber, f => f.Random.AlphaNumeric(10).ToUpper()).RuleFor(h => h.Article, f => $"ART-{f.Random.Number(1000, 9999)}")
+    .RuleFor(h => h.DateCreate, f => f.Date.PastDateOnly(5));
+
+var users = fakerUser.Generate(20);
+var jobTitles = fakerJobTitle.Generate(5);
+var departments = fakerDepartment.Generate(3);
+var softwares = fakerSoftware.Generate(20);
+var hardwares = fakerHardware.Generate(20);
+
+db.Users.AddRange(users);
+db.JobTitles.AddRange(jobTitles);
+db.Departments.AddRange(departments);
+db.Softwares.AddRange(softwares);
+db.Hardwares.AddRange(hardwares);
+
+db.SaveChanges();
+
+Console.WriteLine("Готово!\r\nНажмите на любую кнопку!");
+Console.ReadLine();
