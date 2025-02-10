8c584d79ab2c4df32ec6599a822fd584900fcea9
Артем Селиванов <temchik1997@yandex.ru>
2025-02-10 10:12:36 +0300

    ReportGenerator: Вынес магические числа размера шрифта в константу, убрал лишнее свойство

--- ReportGenerator/Strategy/Акт со 1.3/Act_1_3_Report.cs
+++ ReportGenerator/Strategy/Акт со 1.3/Act_1_3_Report.cs
@@ -7,9 +7,9 @@ namespace ReportGenerator.Strategy.Act_so_1_3;
 
 public class Act_1_3_Report(ReportInfo act) : Report(act)
 {
-    public Act_1_3Info Act_1_3Info => act as Act_1_3Info;
+    private const int _tablesFontSize = 12;
 
-    public override IEnumerable<TableInfo> GetTablesInfo() => Act_1_3Info.TablesInfo;
+    public override IEnumerable<TableInfo> GetTablesInfo() => act.TablesInfo;
 
     public override void FillTableRow(XWPFTableRow row, RowInfo rowInfo)
     {
@@ -28,23 +28,23 @@ public class Act_1_3_Report(ReportInfo act) : Report(act)
 
     private void FillTable1RowCore(XWPFTableRow row, Act_1_3Table1RowInfo dataRow)
     {
-        row.GetCell(CellsTable1.Number).FillCellWithStyle(dataRow.Number.ToString(), 12, ParagraphAlignment.CENTER);
-        row.GetCell(CellsTable1.Name).FillCellWithStyle(dataRow.Name, 12, ParagraphAlignment.CENTER);
-        row.GetCell(CellsTable1.SerialNumber).FillCellWithStyle(dataRow.SerialNumber, 12, ParagraphAlignment.CENTER);
-        row.GetCell(CellsTable1.Article).FillCellWithStyle(dataRow.Article, 12, ParagraphAlignment.CENTER);
-        row.GetCell(CellsTable1.CreateDate).FillCellWithStyle(dataRow.DateCreate, 12, ParagraphAlignment.CENTER);
-        row.GetCell(CellsTable1.InstallPlace).FillCellWithStyle(dataRow.InstallPlace, 12, ParagraphAlignment.CENTER);
-        row.GetCell(CellsTable1.SoftwareName).FillCellWithStyle(dataRow.SoftwareName, 12, ParagraphAlignment.CENTER);
-        row.GetCell(CellsTable1.SoftwareVersion).FillCellWithStyle(dataRow.SoftwareVersion, 12, ParagraphAlignment.CENTER);
-        row.GetCell(CellsTable1.User).FillCellWithStyle(dataRow.User, 12, ParagraphAlignment.CENTER);
+        row.GetCell(CellsTable1.Number).FillCellWithStyle(dataRow.Number.ToString(), _tablesFontSize, ParagraphAlignment.CENTER);
+        row.GetCell(CellsTable1.Name).FillCellWithStyle(dataRow.Name, _tablesFontSize, ParagraphAlignment.CENTER);
+        row.GetCell(CellsTable1.SerialNumber).FillCellWithStyle(dataRow.SerialNumber, _tablesFontSize, ParagraphAlignment.CENTER);
+        row.GetCell(CellsTable1.Article).FillCellWithStyle(dataRow.Article, _tablesFontSize, ParagraphAlignment.CENTER);
+        row.GetCell(CellsTable1.CreateDate).FillCellWithStyle(dataRow.DateCreate, _tablesFontSize, ParagraphAlignment.CENTER);
+        row.GetCell(CellsTable1.InstallPlace).FillCellWithStyle(dataRow.InstallPlace, _tablesFontSize, ParagraphAlignment.CENTER);
+        row.GetCell(CellsTable1.SoftwareName).FillCellWithStyle(dataRow.SoftwareName, _tablesFontSize, ParagraphAlignment.CENTER);
+        row.GetCell(CellsTable1.SoftwareVersion).FillCellWithStyle(dataRow.SoftwareVersion, _tablesFontSize, ParagraphAlignment.CENTER);
+        row.GetCell(CellsTable1.User).FillCellWithStyle(dataRow.User, _tablesFontSize, ParagraphAlignment.CENTER);
     }
 
     private void FillTable2RowCore(XWPFTableRow row, Act_1_3Table2RowInfo dataRow)
     {
-        row.GetCell(CellsTable2.Number).FillCellWithStyle(dataRow.Number.ToString(), 12, ParagraphAlignment.CENTER);
-        row.GetCell(CellsTable2.Name).FillCellWithStyle(dataRow.Name, 12, ParagraphAlignment.CENTER);
-        row.GetCell(CellsTable2.SerialNumber).FillCellWithStyle(dataRow.SerialNumber, 12, ParagraphAlignment.CENTER);
-        row.GetCell(CellsTable2.IncomingInspectionResults).FillCellWithStyle(dataRow.IncomingInspectionResults, 12, ParagraphAlignment.CENTER);
-        row.GetCell(CellsTable2.FunctioningInspectionResults).FillCellWithStyle(dataRow.FunctioningInspectionResults, 12, ParagraphAlignment.CENTER);
+        row.GetCell(CellsTable2.Number).FillCellWithStyle(dataRow.Number.ToString(), _tablesFontSize, ParagraphAlignment.CENTER);
+        row.GetCell(CellsTable2.Name).FillCellWithStyle(dataRow.Name, _tablesFontSize, ParagraphAlignment.CENTER);
+        row.GetCell(CellsTable2.SerialNumber).FillCellWithStyle(dataRow.SerialNumber, _tablesFontSize, ParagraphAlignment.CENTER);
+        row.GetCell(CellsTable2.IncomingInspectionResults).FillCellWithStyle(dataRow.IncomingInspectionResults, _tablesFontSize, ParagraphAlignment.CENTER);
+        row.GetCell(CellsTable2.FunctioningInspectionResults).FillCellWithStyle(dataRow.FunctioningInspectionResults, _tablesFontSize, ParagraphAlignment.CENTER);
     }
 }
